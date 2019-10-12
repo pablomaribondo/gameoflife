1a2968811b5327f8bc55c5b566764f660dba59be
Pablo Maribondo <pablomgvieira@gmail.com>
2019-10-12 16:58:09 -0300

    Main component created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<p>Game of Life</p>, document.getElementById('root'));
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      generation: 0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The Game of Life</h1>
+        {/* <Grid /> */}
+        <h2>Generations: { this.state.generation }</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
