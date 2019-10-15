f01b0da810ee8e767c8c66a8e4ba22a4dbce837f
Pablo Maribondo <pablomgvieira@gmail.com>
2019-10-15 15:17:49 -0300

    Play button & game logic

--- src/index.js
+++ src/index.js
@@ -76,6 +76,7 @@ class Main extends Component {
 
   seed = () => {
     let gridCopy = arrayClone(this.state.gridFull);
+
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
         if (Math.floor(Math.random() * 4) === 1) {
@@ -83,6 +84,7 @@ class Main extends Component {
         }
       }
     }
+
     this.setState({
       gridFull: gridCopy
     })
@@ -93,8 +95,39 @@ class Main extends Component {
     this.intervalId = setInterval(this.play, this.speed);
   }
 
+  pauseButton = () => {
+    clearInterval(this.intervalId);
+  }
+
+  play = () => {
+    let g = this.state.gridFull;
+    let g2 = arrayClone(this.state.gridFull);
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let count = 0;
+        if (i > 0) if (g[i - 1][j]) count++;
+		    if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+		    if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+		    if (j < this.cols - 1) if (g[i][j + 1]) count++;
+		    if (j > 0) if (g[i][j - 1]) count++;
+		    if (i < this.rows - 1) if (g[i + 1][j]) count++;
+		    if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+		    if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+		    if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+		    if (!g[i][j] && count === 3) g2[i][j] = true;
+      }
+    }
+
+    this.setState({
+      gridFull: g2,
+      generation: this.state.generation + 1
+    })
+  }
+
   componentDidMount() {
     this.seed();
+    this.playButton();
   }
 
   render() {
